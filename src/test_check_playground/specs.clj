93bcf359927f20ffafd712500d4e1d453bf71122
Eric Normand <ericwnormand@gmail.com>
2019-10-09 14:32:15 -0500

    Remove ranged-rand

--- src/test_check_playground/specs.clj
+++ src/test_check_playground/specs.clj
@@ -1,7 +1,3 @@
 (ns test-check-playground.specs
   (:require [clojure.spec.alpha :as s]))
 
-(defn ranged-rand  ;; BROKEN!
-  "Returns random int in range start <= rand < end"
-  [start end]
-  (+ start (long (rand (- start end)))))
