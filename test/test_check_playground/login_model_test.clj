46b2a02e6fb8967aec3fa026147ec8aa6a68ed98
Eric Normand <ericwnormand@gmail.com>
2019-10-17 09:10:32 -0500

    Beginning of login-model-test

---
+++ test/test_check_playground/login_model_test.clj
@@ -0,0 +1,9 @@
+(ns test-check-playground.login-model-test
+  (:require [test-check-playground.fn-specs :as sut]
+            [clojure.test :refer [deftest is]]
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.generators :as gen]))
+
+
+
